d5aca1a7e5445ff35b35f534528e2f96d52bc02c
RaviGarbujaPun <cbpmgr@gmail.com>
2017-07-17 16:39:08 +0545

    Almost worked

--- app/src/main/java/np/com/ravi/dbconn/MainActivity.java
+++ app/src/main/java/np/com/ravi/dbconn/MainActivity.java
@@ -1,13 +1,40 @@
 package np.com.ravi.dbconn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnViewProducts;
+    Button btnNewProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnViewProducts = (Button) findViewById(R.id.btnViewProducts);
+        btnNewProduct = (Button) findViewById(R.id.btnCreateProduct);
+
+        btnViewProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //launching all products activity
+                Intent intent= new Intent(getApplicationContext(),AllProductsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btnNewProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NewProductActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
